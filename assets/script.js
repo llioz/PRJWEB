2e8e36cfa21691d63c2af1507c1399cb9a768e7a
Lioz David <Lioz@Liozs-MBP.fritz.box>
2018-01-15 22:02:55 +0100

    aan lijst toevoegen micro interactie

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+var i = 0;
+
+for (i = 1; i < 4; i++) { 
+    document.getElementById("addToListButton" + i).onclick = function() {iconFunction(this.id)};
+}
+
+function iconFunction(buttonId) {
+    console.log(buttonId);
+    var j = 0;
+    for (i = 1; i < 4; i++) {
+        if (buttonId == ("addToListButton" + i)) {
+            if (document.getElementById("icon" + i).className == "fa fa-plus") {
+            document.getElementById("icon" + i).className = "fa fa-floppy-o bounce";
+            } else {
+                document.getElementById("icon" + i).className = "fa fa-plus";
+            }
+        }
+    }
+}
