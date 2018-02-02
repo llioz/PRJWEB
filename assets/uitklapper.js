54eb7f07d777d7d1ca8085b8bf16908d9a6a7b98
Lioz David <Lioz@106.187.028.145.hva.nl>
2018-02-02 09:53:03 +0100

    --
    --

--- assets/uitklapper.js
+++ assets/uitklapper.js
@@ -1,10 +1,9 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.querySelector('div').classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
+
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
 
